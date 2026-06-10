744ca64363c0384b384746023acff58d27ae7c2f
Alok kumar sahu <a7l7o3k5@gmail.com>
2026-06-10 22:40:59 +0530

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
